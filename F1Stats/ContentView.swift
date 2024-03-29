39a6817a8640e9ab11ad446120d9d94d42ea8d5d
Guilherme Carvalho <guilherme.carvalho@vao.pl>
2024-03-29 13:56:26 +0100

    Created repo

---
+++ F1Stats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  F1Stats
+//
+//  Created by Guilherme Carvalho on 29/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ F1Stats/F1StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1StatsApp.swift
+//  F1Stats
+//
+//  Created by Guilherme Carvalho on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct F1StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
