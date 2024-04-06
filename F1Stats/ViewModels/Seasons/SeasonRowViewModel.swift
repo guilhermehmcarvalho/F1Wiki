cd4ed696d542428bb76ca180cfae3ba0f34283cc
Guilherme Carvalho <guilherme.carvalho@vao.pl>
2024-04-06 09:03:17 +0200

    Seasons view models

---
+++ F1Stats/ViewModels/Seasons/SeasonRowViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  SeasonRowViewModel.swift
+//  F1Stats
+//
+//  Created by Guilherme Carvalho on 06/04/2024.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class SeasonRowViewModel: ObservableObject {
+  internal let season: SeasonModel
+  private var apiSeason: APISeasonsProtocol
+  @Published internal var wikipediaViewModel: WikipediaViewModel
+
+  init(season: SeasonModel, wikipediaApi: WikipediaAPIProtocol, apiSeason: APISeasonsProtocol) {
+    self.season = season
+    self.apiSeason = apiSeason
+    self.wikipediaViewModel = WikipediaViewModel(url: season.url, wikipediaApi: wikipediaApi)
+  }
+
+  internal func onTap(isExpanded: Bool) {
+    if isExpanded {
+      wikipediaViewModel.fetchSummary()
+    }
+  }
+//
+//  internal var constructorStandingsRowViewModel: ConstructorStandingsRowViewModel {
+//    ConstructorStandingsRowViewModel(constructorId: constructor.constructorID, apiConstructors: apiConstructor)
+//  }
+}
